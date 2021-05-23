012f6ed607b85b3efa99d9ee318d22bb2d75c903
TAOLIU <liutaooliver@gmail.com>
2021-05-23 18:00:59 +0800

    update

--- src/test/giHomeNBTest.spec.js
+++ src/test/giHomeNBTest.spec.js
@@ -30,7 +30,21 @@ describe('Checkboxes test suite: ', () => {
         // find element by text
         const page1Nextbtn = $('button=Choose plan');
         console.log(page1Nextbtn.getText());
-        page1Nextbtn.click();
+
+        var ind1next = 0;
+        while(ind1next < 5) {
+
+            if(page1Nextbtn.isClickable()) {
+                console.log("page 1 next btn clickable");
+                page1Nextbtn.click();
+                break;
+            }else {
+                console.log("page 1 next btn NOT clickable, retry");
+                browser.pause(5000);
+                ind1next ++;
+            }
+        }
+
 
         browser.pause(5000);
 
@@ -38,7 +52,21 @@ describe('Checkboxes test suite: ', () => {
         // find element by text
         const page2NextBtn = $('a=Select');
         console.log(page2NextBtn.getText());
-        page2NextBtn.click();
+    
+        var ind2next = 0;
+        while(ind2next < 5) {
+
+            if(page2NextBtn.isClickable()) {
+                console.log("page 2 next btn clickable");
+                page2NextBtn.click();
+                break;
+            }else {
+                console.log("page 2 next btn NOT clickable, retry");
+                browser.pause(5000);
+                ind2next ++;
+            }
+        }
+
 
         browser.pause(5000);
 
@@ -46,7 +74,21 @@ describe('Checkboxes test suite: ', () => {
         // find element by text
         const page3Nextbtn = $('a=Go to personal details');
         console.log(page3Nextbtn.getText());
-        page3Nextbtn.click();
+        
+        var ind3next = 0;
+        while(ind3next < 5) {
+
+            if(page3Nextbtn.isClickable()) {
+                console.log("page 3 next btn clickable");
+                page3Nextbtn.click();
+                break;
+            }else {
+                console.log("page 3 next btn NOT clickable, retry");
+                browser.pause(5000);
+                ind3next ++;
+            }
+        }
+
 
         browser.pause(5000);
 
@@ -66,10 +108,26 @@ describe('Checkboxes test suite: ', () => {
         page4Givenname.setValue('t');
         console.log(page4Givenname.getValue()); 
 
+        browser.pause(5000);
+
         // find element by text
-        const page4Geder = $('span=Male');
-        console.log(page4Geder.getText());
-        page4Geder.click();
+        const page4Gender = $('span=Male');
+        console.log(page4Gender.getText());
+        
+        var ind4Gender = 0;
+        while(ind4Gender < 5) {
+
+            if(page4Gender.isClickable()) {
+                console.log("page 4 gender btn clickable");
+                page4Gender.click();
+                break;
+            }else {
+                console.log("page 4 gender btn NOT clickable, retry");
+                browser.pause(5000);
+                ind4Gender ++;
+            }
+        }
+
 
         //find elememt by name
         const page4Nric = $('[name="identificationNo"]')
@@ -81,12 +139,154 @@ describe('Checkboxes test suite: ', () => {
         dob.$$('li')[1].$('input').setValue('12');
         dob.$$('li')[2].$('input').setValue('1982');
 
+        
+        const page4Mobile= $('#mobile');
+        page4Mobile.setValue('93755748');
+        console.log(page4Mobile.getValue());
+
+        const page4Email= $('#email');
+        page4Email.setValue('tao_liu@aviva-asia.com');
+        console.log(page4Email.getValue());
+        
+
+        const page4PostalCode= $('#postalCode');
+        page4PostalCode.setValue('160009');
+        console.log(page4PostalCode.getValue());
+
+        browser.pause(5000);
+        
+        const page4Addressbtn = $('a=Find my address');
+        console.log(page4Addressbtn.getText()); 
+
+        var indaddress = 0;
+        while(indaddress < 5) {
+
+            if(page4Addressbtn.isClickable()) {
+                console.log("QAS Address btn clickable");
+                page4Addressbtn.click();
+                break;
+            }else {
+                console.log("QAS Address btn NOT clickable, retry");
+                browser.pause(5000);
+                indaddress ++;
+            }
+        }
+
         browser.pause(5000);
 
+        const page4PBlock= $('#addressLine1');
+        const page4PStreet= $('#addressLine2');
+        const page4PBuilding= $('#addressLine4');
+        var block = page4PBlock.getValue();
+        if(block === null || block === '') {
+
+            console.log("QAS API NOT working, manually fill in adress");
+            page4PBlock.setValue('1');
+            page4PStreet.setValue('street test');
+            page4PBuilding.setValue('building test');
+
+        }
+        console.log(page4PBlock.getValue());
+        console.log(page4PStreet.getValue());
+        console.log(page4PBlock.getValue());
+
+        const page4MailAddressOption = $('span=No');
+        console.log(page4MailAddressOption.getText());
+        
+
+        var ind4MailAd = 0;
+        while(ind4MailAd < 5) {
+
+            if(page4Gender.isClickable()) {
+                console.log("page 4 mail address option btn clickable");
+                page4MailAddressOption.click();
+                break;
+            }else {
+                console.log("page 4 mail address option btn NOT clickable, retry");
+                browser.pause(5000);
+                ind4MailAd ++;
+            }
+        }
 
 
-        browser.pause(15000);
+        const page4Next = $('button=Go to summary & payment');
+        console.log(page4Next.getText());
 
+        var ind4next = 0;
+        while(ind4next < 5) {
+
+            if(page4Next.isClickable()) {
+                console.log("page 4 next btn clickable");
+                page4Next.click();
+                break;
+            }else {
+                console.log("page 4 next btn NOT clickable, retry");
+                browser.pause(5000);
+                ind4next ++;
+            }
+        }
+
+        browser.pause(10000);
+
+        //////page 5//////
+        const page5Nextbtn = $('a=I agree - buy now');
+        console.log(page5Nextbtn.getText());
+        
+
+        var ind5next = 0;
+        while(ind5next < 5) {
+
+            if(page5Nextbtn.isClickable()) {
+                console.log("page 5 next btn clickable");
+                page5Nextbtn.click();
+                break;
+            }else {
+                console.log("page 5 next btn NOT clickable, retry");
+                browser.pause(5000);
+                ind5next ++;
+            }
+        }
+
+
+        
+        //////page 6//////
+        const page6PaymentMode= $('h3=Pay with Visa or MasterCard');
+        console.log(page6PaymentMode.getText());
+        var ind6PaymentMode = 0;
+        while(ind6PaymentMode < 5) {
+
+            if(page6PaymentMode.isClickable()) {
+                console.log("page 6 Pay with Visa or MasterCard btn clickable");
+                page6PaymentMode.click();
+                break;
+            }else {
+                console.log("page 6 Pay with Visa or MasterCard btn NOT clickable, retry");
+                browser.pause(5000);
+                ind6PaymentMode ++;
+            }
+        }
+
+        browser.pause(3000);
+
+        const page6NextBtn = $('a=Proceed to payment');
+        console.log(page6NextBtn.getText());
+
+        var ind6Next = 0;
+        while(ind6Next < 5) {
+
+            if(page6NextBtn.isClickable()) {
+                console.log("page 6 next btn clickable");
+                page6NextBtn.click();
+                break;
+            }else {
+                console.log("page 6 next btn NOT clickable, retry");
+                browser.pause(5000);
+                ind6Next ++;
+            }
+        }
+
+
+        browser.pause(600000);
 
     });
 });
